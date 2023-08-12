1e503e18e008945241b23eccbc7652bd07ea6f5c
tahirivazquez <tahiwdev@gmail.com>
2023-08-12 17:00:14 -0300

    fetch
    seguimos en el intento

--- js/products.js
+++ js/products.js
@@ -1,9 +1,45 @@
-const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json"; 
+/*const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json"; 
 
 const container = document.getElementByClassName("container"); 
 
+/*function showProducts(productsArray) {
+  for (const product of productsArray) {
+      const productDiv = document.createElement("div");
+      productDiv.classList.add("product");
 
-/*fetch(DATA_URL)
+      const productImage = document.createElement("img");
+      productImage.src = product.image;
+      productDiv.appendChild(productImage);
+
+      const productName = document.createElement("h2");
+      productName.textContent = product.name;
+      productDiv.appendChild(productName);
+
+      const productDescription = document.createElement("p");
+      productDescription.textContent = product.description;
+      productDiv.appendChild(productDescription);
+
+      const productPrice = document.createElement("p");
+      productPrice.textContent = `Precio: ${product.price}`;
+      productDiv.appendChild(productPrice);
+
+      const productSold = document.createElement("p");
+      productSold.textContent = `Vendidos: ${product.sold}`;
+      productDiv.appendChild(productSold);
+
+      container.appendChild(productDiv);
+  }
+}
+
+fetch(API_URL)
+  .then(response => response.json())
+  .then(data => {
+      const products = data.products;
+      showProducts(products);
+  })
+  .catch(error => console.error("Error:", error));
+
+fetch(DATA_URL)
 .then(function (response) {
   return response.json();
 })
