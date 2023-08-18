0f6844f0911024e6abfbf89935e366cefd9cdfab
dbustillo11 <117930537+dbustillo11@users.noreply.github.com>
2023-08-17 22:30:52 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,15 +1,25 @@
-function isAuthenticated() {
-  return localStorage.getItem('authenticated') === 'true';
-}
-
 function authenticate() {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
   if (username && password) {
-  
+
     localStorage.setItem('authenticated', 'true'); 
-    window.location.href = 'index.html'; 
+    window.location.href = 'index.html';
+  } else {
     alert('Por favor ingrese un usuario y contraseña válidos.');
   }
 }
+
+// Verificar la autenticación al cargar la página
+window.addEventListener('load', function() {
+  if (!isAuthenticated()) {
+
+    window.location.href = 'login.html';
+  }
+});
+
+function isAuthenticated() {
+  return localStorage.getItem('authenticated') === 'true';
+}
+
